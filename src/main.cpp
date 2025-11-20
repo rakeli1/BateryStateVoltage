8a671dc9e15c0b6dc31e8a3281c4cd28bcc41bcc
Краснобаев Алексей <rakeliit@protonmail.com>
2025-11-21 01:36:45 +0200

    Отладочная версия программы для обработки заряда батареи без комментариев

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,15 @@
 
 
  const int BATPIN = A0;
- const float INTERNREF = 1.1f;
+ const float INREF = 1.1f;
  const float R1 = 0;
  const float R2 = 0;
  const float RATIO = (R1 + R2) / R2;
  const float VMIN = 3.3f;
  const float VMAX = 4.2f;
  const float K = R2 / (R1 + R2);
-
+       float Va0 = 0;
+       float Vbat = 0;
           
 
 void setup() 
@@ -21,15 +22,31 @@ void setup()
 
 void loop() 
 {
-  int adc = 0;
+  int adc = 0;                           //число формируемое АЦП 
   
   for(int sempl = 0; sempl < 20; ++sempl)
   {
      adc += analogRead(BATPIN);
       delayMicroseconds(50);
   }
-  adc /= 20;  
+  adc /= 20;
+  Va0 = adc * (INREF / 1023);
+  Vbat = (adc * (INREF / 1023)) * RATIO;
+  int percent = ((Vbat - VMIN) / (VMAX - VMIN)) * 100;
+
   Serial.println(adc);   //подключить конденсатор паралельно нижнему резистору 0.01-0.1мкФ
+
+  // Напряжение на батарее
+  Serial.print("Напряжение на батарее ");
+  Serial.println(Vbat);
+
+  // Напряжение на пине А0
+  Serial.print("Напряжение на пине A0 ");
+  Serial.println(Va0);
+
+  // Процент заряда батареи
+  Serial.print("Процент заряда батареи ");
+  Serial.println(percent);
   delay(1000);
 }
 
