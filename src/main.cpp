969ccf24f96a57e03feb75ff433c13a23ef83df2
Краснобаев Алексей <rakeliit@protonmail.com>
2025-11-21 23:35:00 +0200

    Окончательная отладочная версия программы обрабатывающей состояние батареи

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,16 @@
 #include <Arduino.h>
 
 
- const int BATPIN = A0;
- const float INREF = 1.1f;
- const float R1 = 0;
- const float R2 = 0;
- const float RATIO = (R1 + R2) / R2;
- const float VMIN = 3.3f;
- const float VMAX = 4.2f;
- const float K = R2 / (R1 + R2);
-       float Va0 = 0;
-       float Vbat = 0;
+ const int BATPIN = A0;              // Определение пина А0 для входа от делителя батарейного напряжения 
+ const float INREF = 1.1f;           // внутреннее опорное напряжение подключается функцией analogReference(INTERNAL);
+ const float R1 = 3;                 // R1 - подключен м-у Vbat и пином A0 
+ const float R2 = 1;                 // R2 - покдключен м-у GND
+ const float RATIO = (R1 + R2) / R2; // коефицент увеличения напряжения выхода
+ const float VMIN = 3.3f;            // значение напряжения батареи при котором считать уровень зарядки "0"
+ const float VMAX = 4.2f;            // значение напряжения батареи при котором считать уровень заряда 100%
+ const float K = R2 / (R1 + R2);     // коефицент затухания делителя
+       float Va0 = 0;                // значение напряжения на пине А0; расчетное по значению от analogread(BATPIN)
+       float Vbat = 0;               //  расчетное значение напряжения на батарее (подлежит калибровке)
           
 
 void setup() 
@@ -24,29 +24,31 @@ void loop()
 {
   int adc = 0;                           //число формируемое АЦП 
   
-  for(int sempl = 0; sempl < 20; ++sempl)
+  for(int sempl = 0; sempl < 20; ++sempl) // цикл для  усреднения значения АЦП
   {
      adc += analogRead(BATPIN);
       delayMicroseconds(50);
   }
-  adc /= 20;
-  Va0 = adc * (INREF / 1023);
-  Vbat = (adc * (INREF / 1023)) * RATIO;
-  int percent = ((Vbat - VMIN) / (VMAX - VMIN)) * 100;
+  adc /= 20;                              // уреднение значения АЦП
+  Va0 = adc * (INREF / 1023);             // вычисление напряжения на пине А0 
+  Vbat = (adc * (INREF / 1023)) * RATIO;  // вычисление напряжения на батарее
+  int percent = ((Vbat - VMIN) / (VMAX - VMIN)) * 100; // вычисление процента зарядки батареи
 
+  Serial.print("Значение АЦП           ");
   Serial.println(adc);   //подключить конденсатор паралельно нижнему резистору 0.01-0.1мкФ
 
   // Напряжение на батарее
-  Serial.print("Напряжение на батарее ");
+  Serial.print("Напряжение на батарее  ");
   Serial.println(Vbat);
 
   // Напряжение на пине А0
-  Serial.print("Напряжение на пине A0 ");
+  Serial.print("Напряжение на пине A0  ");
   Serial.println(Va0);
 
   // Процент заряда батареи
   Serial.print("Процент заряда батареи ");
   Serial.println(percent);
-  delay(1000);
+  Serial.println();
+  delay(5000);
 }
 
